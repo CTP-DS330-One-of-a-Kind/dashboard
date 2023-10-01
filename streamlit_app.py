5d8ff955972cefb251d5687a35b2772619797e09
Richard Palestri <rpalestri@aol.com>
2023-10-01 18:23:26 -0400

    filter slider works with df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,25 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import geopandas as gpd
-
 
 def main():
-    st.title("Streamlit App Template")
+    st.title("One of a Kind")
 
     df = pd.read_csv("data/MW.csv", encoding='cp1252')
-
     df_state = pd.read_csv("data/state.csv", encoding="unicode_escape")
-    
-    # Add your content here
-    st.write("THE DATA")
-    st.dataframe(df)
-    
-    # Example: Adding a button
-    if st.button("Click me"):
-        st.write("Button clicked!")
-    
-    # Example: Getting user input
-    user_input = st.text_input("Enter something:")
-    st.write("You entered:", user_input)
-    
-    # Example: Displaying data
-    st.write("Displaying a DataFrame:")
-    data = {'col1': [1, 2, 3],
-            'col2': [10, 20, 30]}
-    df = pd.DataFrame(data)
-    st.write(df)
-    
-    # Example: Plotting a chart
-    st.write("Plotting a chart:")
-    chart_data = {'x': [1, 2, 3, 4, 5],
-                  'y': [10, 20, 15, 25, 30]}
-    chart_df = pd.DataFrame(chart_data)
-    st.line_chart(chart_df)
+    df['State'] = df['State'].str.lower()
+    # Convert column names to lowercase to match both datasets before merging
+    df.columns = df.columns.str.lower()
+
+    # Merge the DataFrames
+    merged_df = pd.merge(df, df_state[['state', 'year', 'governor']], left_on=['state', 'year'], right_on=['state', 'year'])
+
+    # Filter data based on selected year
+    selected_year = st.slider("Select a year:", min_value=1980, max_value=2020, value=1980, step=1)
+    filtered_df = merged_df[merged_df['year'] == selected_year]
+
+    # Display the filtered DataFrame
+    st.title("Filtered Data")
+    st.dataframe(filtered_df)
 
 if __name__ == "__main__":
     main()
