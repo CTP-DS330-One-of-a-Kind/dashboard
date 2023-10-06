1cefd0f2a33304be1b363c4a3696c56ded46fc94
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2023-10-06 05:58:56 -0400

    combine all graphs

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,12 @@ import geopandas as gpd
 from matplotlib import pyplot as plt
 
 def main():
+
+    # Set Page Format
+    #st.set_page_config(layout="wide") 
     st.title("One of a Kind")
 
+    # Read Data
     df = pd.read_csv("data/MW.csv", encoding='cp1252')
     states = gpd.read_file('shape/cb_2018_us_state_5m.shp')
     
@@ -17,13 +21,22 @@ def main():
     # Merge the DataFrames
     merged_df = pd.merge(df, df_state[['state', 'year', 'governor']], left_on=['state', 'year'], right_on=['state', 'year'])
 
-    # Filter data based on selected year
-    selected_year = st.slider("Select a year:", min_value=1980, max_value=2020, value=1980, step=1)
+    # Sidebar for filtering/sorting
+    with st.sidebar: 
+        # Filter data based on selected year
+        st.subheader("Filter Data")
+        selected_year = st.slider("Select a year:", min_value=1980, max_value=2016, value=1980, step=1)
+
     filtered_df = merged_df[merged_df['year'] == selected_year]
+    
+    colors = filtered_df['governor'].apply(lambda x: 'blue' if x == 'D' else 'red')
 
     filtered_df_unmerged = df[df['year'] == selected_year]
     states['wage'] = states['NAME'].str.lower().map(filtered_df_unmerged.set_index('state')['effective.minimum.wage.2020.dollars'])
 
+
+    # Map
+    st.subheader("Effective Minimum Wage by State (Adjusted to 2020 inflation)")
     fig, ax = plt.subplots(figsize=(10, 8))  # Adjust the figsize as needed
     ax.set_axis_off()
     ax.set_xlim(-126, -66) 
@@ -31,16 +44,36 @@ def main():
     
     
     LEGEND_MIN = 5
-    LEGEND_MAX = 17.5
+    LEGEND_MAX = 20
 
     states.plot(column="wage", ax=ax, linewidth=0.5,    edgecolor='0.8', 
-                cmap='Greens', legend=True, vmin=LEGEND_MIN, vmax=LEGEND_MAX) 
+                cmap='cividis', legend=True, vmin=LEGEND_MIN, vmax=LEGEND_MAX) 
     
     st.pyplot(fig)  
 
-    st.subheader(f'Data for the year {selected_year}')
-    st.write(filtered_df)
+    # Bar plot: Minimum Wage by State
+    st.subheader("Bar Plot: Minimum Wage by State")
+    plt.figure(figsize=(7, 12)) 
 
+    # Sort the DataFrame by 'state' for correct correlation
+    filtered_df_state = filtered_df.sort_values(by='state')
+
+    
+    BAR_MIN = 0
+    BAR_MAX = 20
+
+    plt.barh(filtered_df_state['state'], filtered_df_state['state.minimum.wage'], color=colors)
+    plt.title('Minimum Wage by State')
+    plt.xlabel('Minimum Wage U$D')
+    plt.ylabel('State')
+    plt.xlim(0, 18)
+    plt.gca().invert_yaxis()
+
+    st.pyplot(plt)
+
+    # Display the filtered DataFrame
+    st.subheader("Filtered Data")
+    st.dataframe(filtered_df)
 
 
    
