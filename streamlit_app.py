a497731c3bfa28bd58f87cadfa10910bcfecdbc8
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2023-10-05 23:11:45 -0400

    added map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import geopandas as gpd
 from matplotlib import pyplot as plt
 
 def main():
     st.title("One of a Kind")
 
     df = pd.read_csv("data/MW.csv", encoding='cp1252')
+    states = gpd.read_file('shape/cb_2018_us_state_5m.shp')
+    
     df_state = pd.read_csv("data/state.csv", encoding="unicode_escape")
     df['State'] = df['State'].str.lower()
     # Convert column names to lowercase to match both datasets before merging
@@ -19,42 +21,29 @@ def main():
     selected_year = st.slider("Select a year:", min_value=1980, max_value=2020, value=1980, step=1)
     filtered_df = merged_df[merged_df['year'] == selected_year]
 
-    # Display the filtered DataFrame
-    st.title("Filtered Data")
-    st.dataframe(filtered_df)
+    filtered_df_unmerged = df[df['year'] == selected_year]
+    states['wage'] = states['NAME'].str.lower().map(filtered_df_unmerged.set_index('state')['effective.minimum.wage.2020.dollars'])
 
-    st.write(df.columns)
-
-    xaxis = df["state"].values
+    fig, ax = plt.subplots(figsize=(10, 8))  # Adjust the figsize as needed
+    ax.set_axis_off()
+    ax.set_xlim(-126, -66) 
+    ax.set_ylim(24, 50)
     
-    yaxis=df["effective.minimum.wage"].values
-    import seaborn as sns
-    test = pd.DataFrame(
-    {
-        "x": xaxis,
-        "y": yaxis
-        
-    }
-)
-    plt.figure(figsize=(7, 12)) 
-    sns.barplot(data=test.sort_values('y', ascending=False), y='x', x='y')
-    plt.title('minimum wage every state')
-    plt.xlabel('minimum wage')
-    plt.ylabel('state')
-    plt.show()
+    
+    LEGEND_MIN = 5
+    LEGEND_MAX = 17.5
+
+    states.plot(column="wage", ax=ax, linewidth=0.5,    edgecolor='0.8', 
+                cmap='Greens', legend=True, vmin=LEGEND_MIN, vmax=LEGEND_MAX) 
+    
+    st.pyplot(fig)  
+
+    st.subheader(f'Data for the year {selected_year}')
+    st.write(filtered_df)
 
 
-    fig = px.choropleth(
-                        df,
-                        locations='state', 
-                        locationmode="USA-states", 
-                        scope="usa",
-                        color='effective.minimum.wage',
-                    color_continuous_scale="Viridis_r", 
-                        
-                        )
-    fig.show()
 
+   
 if __name__ == "__main__":
     main()
 
